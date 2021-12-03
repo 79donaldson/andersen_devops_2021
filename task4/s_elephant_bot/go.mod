b940cc5fba4ce5328547ec02b1255202e9aebcf4
Aleksey <79donaldson@gmail.com>
2021-12-03 18:52:16 +0200

    update main

--- task4/s_elephant_bot/go.mod
+++ task4/s_elephant_bot/go.mod
@@ -7,4 +7,5 @@ require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.4.0-beta.0
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/telegram-bot-api.v4 v4.6.4 // indirect
 )
--- task4/s_elephant_bot/main.go
+++ task4/s_elephant_bot/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	//	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	//"gopkg.in/telegram-bot-api.v4"
 )
 
+//type Config struct {
+//	tgbotapikey string
+//}
 const tgbotapikey = "2137759225:AAF_bU-jiHY0-TCPwXZcLQfWFGeQabemLaU"
 
 func main() {
 	var (
+		//file, _ := os.Open("config.json")
+		//decoder := json.NewDecoder(file)
+		//configuration := Config{}
+		//err := decoder.Decode(&configuration)
+		//if err != nil {
+		//		log.Panic(err)
 		bot        *tgbotapi.BotAPI
 		err        error
 		updChannel tgbotapi.UpdatesChannel
@@ -39,34 +51,41 @@ func main() {
 
 			if update.Message.IsCommand() {
 				cmdText := update.Message.Command()
-				if cmdText == "Git" {
+				if strings.ToLower(cmdText) == "git" {
 					msgConfig := tgbotapi.NewMessage(
 						update.Message.Chat.ID,
 						"https://github.com/79donaldson/andersen_devops_2021")
 					bot.Send(msgConfig)
-				} else if cmdText == "Task1" {
+				} else if strings.ToLower(cmdText) == "tasks" {
+					msgConfig := tgbotapi.NewMessage(
+						update.Message.Chat.ID,
+						"https://github.com/79donaldson/andersen_devops_2021/tree/main/task1\n"+
+							"https://github.com/79donaldson/andersen_devops_2021/tree/main/task2\n"+
+							"https://github.com/79donaldson/andersen_devops_2021/tree/main/task3\n"+
+							"https://github.com/79donaldson/andersen_devops_2021/tree/main/task4\n")
+					bot.Send(msgConfig)
+				} else if strings.ToLower(cmdText) == "task1" {
 					msgConfig := tgbotapi.NewMessage(
 						update.Message.Chat.ID,
 						"https://github.com/79donaldson/andersen_devops_2021/tree/main/task1")
 					bot.Send(msgConfig)
-				} else if cmdText == "Task2" {
+				} else if strings.ToLower(cmdText) == "task2" {
 					msgConfig := tgbotapi.NewMessage(
 						update.Message.Chat.ID,
 						"https://github.com/79donaldson/andersen_devops_2021/tree/main/task2")
 					bot.Send(msgConfig)
-				} else if cmdText == "Task3" {
+				} else if strings.ToLower(cmdText) == "task3" {
 					msgConfig := tgbotapi.NewMessage(
 						update.Message.Chat.ID,
 						"https://github.com/79donaldson/andersen_devops_2021/tree/main/task3")
 					bot.Send(msgConfig)
-				} else if cmdText == "Task4" {
+				} else if strings.ToLower(cmdText) == "task4" {
 					msgConfig := tgbotapi.NewMessage(
 						update.Message.Chat.ID,
 						"https://github.com/79donaldson/andersen_devops_2021/tree/main/task4")
 					bot.Send(msgConfig)
 				}
 			} else {
-
 				fmt.Printf(
 					"from: %s; chatID: %v; messege: %s\n",
 					update.Message.From.FirstName,
